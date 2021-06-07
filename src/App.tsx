36e91d63dfcd4044392fff1680f8bd719f5b870b
maximus986 <aleksandar.maksimovic2608@gmail.com>
2021-06-07 21:57:45 +0200

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCardsDeck } from 'game/gameSlice';
 import { Game } from 'game/Game';
--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,5 @@
 import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import gameSlice from './game/gameSlice';
-
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export const rootReducer = combineReducers({
   game: gameSlice.reducer,
